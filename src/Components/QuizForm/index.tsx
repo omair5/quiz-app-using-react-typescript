eebb850d6218ca9dc271043fc3c076b5701efcc0
omair5 <muhammad.omair235@gmail.com>
2021-07-16 20:00:00 +0500

    formfield values gathered

--- src/Components/QuizForm/index.tsx
+++ src/Components/QuizForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { useSelector, RootStateOrAny } from 'react-redux';
 
@@ -8,31 +8,54 @@ interface categoryItems {
     name: string
 }
 
+const DifficultyLevel = [
+    { id: 1, name: 'Easy' },
+    { id: 2, name: 'Medium' },
+    { id: 3, name: 'Hard' },
+]
+
 const QuizForm: React.FC = () => {
 
     const Category_select_options = useSelector((state: RootStateOrAny) => state.FormCategory)
-    console.log('this is what i want', Category_select_options)
+    const [formFields, setformFields] = useState({ name: '', category: '', difficulty: '' })
+
+    const HandleFormChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setformFields({ ...formFields, [e.target.name]: e.target.value })
+    }
+
+    const HandleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(formFields)
+    }
 
     return (
         <>
-            <form>
-
+            <form onSubmit={HandleSubmit}>
+                {/* NAME */}
                 <div>
-                    <TextField id="outlined-basic" placeholder='Enter Your Name' variant="outlined" />
+                    <TextField
+                        id="outlined-basic"
+                        placeholder='Enter Your Name'
+                        variant="outlined"
+                        value={formFields.name}
+                        name='name'
+                        onChange={HandleFormChange}
+                    />
                 </div>
 
+                {/* CATEGORY */}
                 <div>
                     <TextField
                         id="filled-select-currency-native"
                         select
-                        label="Select Category"
-                        // value={ }
-                        // onChange={ }
+                        value={formFields.category}
+                        onChange={HandleFormChange}
                         SelectProps={{
                             native: true,
                         }}
                         helperText="Please Select Category"
                         variant="outlined"
+                        name='category'
                     >
                         {
                             Category_select_options.length === 0 ? 'Loading...' :
@@ -45,27 +68,29 @@ const QuizForm: React.FC = () => {
                     </TextField>
                 </div>
 
+                {/* DIFFICULTY LEVEL */}
                 <div>
                     <TextField
                         id="filled-select-currency-native"
                         select
-                        label="Select Difficulty"
-                        // value={ }
-                        // onChange={ }
+                        value={formFields.difficulty}
+                        onChange={HandleFormChange}
                         SelectProps={{
                             native: true,
                         }}
                         helperText="Please select Difficulty"
                         variant="outlined"
+                        name='difficulty'
                     >
-                        {/* {currencies.map((option) => (
-                        <option key={option.value} value={option.value}>
-                            {option.label}
-                        </option>
-                    ))} */}
+                        {DifficultyLevel.map((option: categoryItems, index: number) => (
+                            <option key={index} value={option.name}>
+                                {option.name}
+                            </option>
+                        ))}
                     </TextField>
                 </div>
 
+                <button type='submit'>START QUIZ</button>
             </form>
         </>
     );
