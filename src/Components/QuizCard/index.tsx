04bb2e11ed7b196eb5efea02b3d3901d1bbfdcaf
omair5 <muhammad.omair235@gmail.com>
2021-08-18 22:51:06 +0500

    quiz card

--- src/Components/QuizCard/index.tsx
+++ src/Components/QuizCard/index.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import { useSelector, RootStateOrAny } from 'react-redux';
-import { QuizCardData, FetchQuizResponse } from '../../QuizType';
+import { QuizCardData } from '../../QuizType';
+import { decode } from 'html-entities';
+import Radio from '@material-ui/core/Radio';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import { useState } from 'react';
 
 const QuizCard: React.FC = () => {
-    const QuizData: FetchQuizResponse[] = useSelector((state: RootStateOrAny) => state.QuizData)
-
+    const QuizData: QuizCardData[] = useSelector((state: RootStateOrAny) => state.QuizData)
+    const [option, setoption] = useState<string>('')
     console.log('this is quiz data', QuizData)
     return (
         <>
-            this is quiz card
+            {
+                QuizData.length === 0 ? 'loadinggg' :
+                    // DECODING HTML ENTITIES THROUGH NPM PACKAGE
+                    <div>
+                        <div>
+                            {decode(QuizData[0].question)}
+                        </div>
+
+                        <RadioGroup name="option" >
+                            <FormControlLabel
+                                value="end"
+                                control={<Radio color="primary" />}
+                                label="End"
+                            />
+                        </RadioGroup>
+
+                        <FormHelperText>please select an option</FormHelperText>
+                    </div>
+
+
+            }
+
         </>
+
     );
+
 }
 
 export default React.memo(QuizCard);
