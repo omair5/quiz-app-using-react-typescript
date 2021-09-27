051d8d044841d95d56ef9068fb80d5454f403545
omair5 <muhammadomair.235@gmail.com>
2021-09-27 22:53:27 +0500

    count down timer implemented

--- src/Components/CountDownTimer/index.tsx
+++ src/Components/CountDownTimer/index.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 const CountDownTimer: React.FC = () => {
+    const [min, setmin] = useState<number>(1)
+    const [sec, setsec] = useState<number>(0)
+
+    useEffect(() => {
+        let myInterval = setInterval(() => {
+            if (sec > 0) {
+                setsec(prev => prev - 1)
+            }
+            else {
+                if (min === 0) {
+                    clearInterval(myInterval)
+                }
+                else {
+                    setmin(prev => prev - 1)
+                    setsec(59)
+                }
+
+            }
+        }, 1000)
+        return () => {
+            clearInterval(myInterval)
+
+        }
+    })
+
+    // FOR LEADING ZEROS OF VALUES LESS THAN 10
+    const LeadingZeros = (seconds: number) => {
+        return seconds < 10 ? `0${seconds}` : seconds
+    }
+
     return (
         <>
-            this is timer
+            {`${min}:${LeadingZeros(sec)}`}
         </>
     );
 }
 
-export default CountDownTimer;
+export default React.memo(CountDownTimer);
